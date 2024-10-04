b8f8638247d551f6b8eb3a0b59b8a0d9766211ea
iamdaichi <takanogi2468@gmail.com>
2024-10-04 12:34:53 +0100

    api url revised

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,13 +2,13 @@ import axios from "axios"
 
 // instance
 export default axios.create({
-  baseURL:"https://mern-api.onrender.com"
+  baseURL:"https://mern-api-hokt.onrender.com"
 })
 // baseURL:"http://localhost:3500"
 
 // instance 
 export const axiosPrivate = axios.create({
-  baseURL:"https://mern-api.onrender.com",
+  baseURL:"https://mern-api-hokt.onrender.com",
   headers:{"Content-Type":"application/json"},
   withCredentials:true
 })
