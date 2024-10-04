88ff13dab203a64357f3a0a2df2a8168c65fa489
iamdaichi <takanogi2468@gmail.com>
2024-10-04 12:22:14 +0100

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,13 +2,13 @@ import axios from "axios"
 
 // instance
 export default axios.create({
-  baseURL:"https://mern-api-pnpz.onrender.com"
+  baseURL:"https://mern-api.onrender.com"
 })
 // baseURL:"http://localhost:3500"
 
 // instance 
 export const axiosPrivate = axios.create({
-  baseURL:"https://mern-api-pnpz.onrender.com",
+  baseURL:"https://mern-api.onrender.com",
   headers:{"Content-Type":"application/json"},
   withCredentials:true
 })
